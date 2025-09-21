e23437b88adb423dce218ea4c82b84d474864025
Karel-S <karelsajdak@gmail.com>
2025-09-21 11:08:05 +0900

    fix:　バグを修正

--- TimeRangeChecker.java
+++ TimeRangeChecker.java
@@ -4,7 +4,7 @@ public class TimeRangeChecker {
     private static final int MAX_HOUR = 23;
 
     // ある時刻(0時～23時)が、指定した時間の範囲内に含まれるかどうかを調べる
-    public boolean isTimeInRange(int time, int start, int end) {
+    public static boolean isTimeInRange(int time, int start, int end) {
         if (start == end) {
             return true;
         } else if (start < end) {
--- TimeRangeCheckerApp.java
+++ TimeRangeCheckerApp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TimeRangeApp {
+public class TimeRangeCheckerApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
